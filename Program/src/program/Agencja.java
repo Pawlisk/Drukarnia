3ae6f494822471fe706436aa59a779ae2372091d
Pawlisk <pk_klapsa@o2.pl>
2025-06-16 15:06:08 +0200

    Add XML-based price configuration

--- Program/src/program/Agencja.java
+++ Program/src/program/Agencja.java
@@ -1,30 +1,11 @@
 package program;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 /**
- *
- * @author Paweł
+ * Price configuration for agency customers.
  */
 public class Agencja {
-
-    int f_monomer = 25; //1
-    int f_polimer = 40; //2
-    int ww300 = 45; //3
-    int OWV = 30; //4
-    int rollup = 30; //5
-    int ban_lamin = 25; //6
-    int ban_powl = 28; //7
-    int f_wylewana_3m = 70; //8
-    int papier_130 = 25; //9
-    int lam_monomer = 15; //10
-    int lam_polimer = 25; //11 
-    int lam_wylewany_3m = 60; //12
+    private final PriceManager prices;
+    int[] tab;
     double dl;
     double szer;
     int menu1;
@@ -32,7 +13,24 @@ public class Agencja {
     int menu3 = 0;
     int menu4;
     int menu5 = 1;
-    int[] tab = {f_monomer, f_polimer, ww300, OWV, rollup, ban_lamin, ban_powl, f_wylewana_3m, papier_130, lam_monomer, lam_polimer, lam_wylewany_3m};
+
+    Agencja(PriceManager prices) {
+        this.prices = prices;
+        tab = new int[]{
+            prices.getPrice("agencja.f_monomer"),
+            prices.getPrice("agencja.f_polimer"),
+            prices.getPrice("agencja.ww300"),
+            prices.getPrice("agencja.OWV"),
+            prices.getPrice("agencja.rollup"),
+            prices.getPrice("agencja.ban_lamin"),
+            prices.getPrice("agencja.ban_powl"),
+            prices.getPrice("agencja.f_wylewana_3m"),
+            prices.getPrice("agencja.papier_130"),
+            prices.getPrice("agencja.lam_monomer"),
+            prices.getPrice("agencja.lam_polimer"),
+            prices.getPrice("agencja.lam_wylewany_3m")
+        };
+    }
 
     void printMaterials() {
         System.out.println("Dostępne materiały:\n1. Folia monomer\n2. Folia polimer\n3. WW300\n4. OWV\n5. Rollup\n6. Baner laminowany\n7. Baner powlekany\n8. Folia wylewana 3m\n9. Papier 130g");
@@ -49,5 +47,4 @@ public class Agencja {
     void setWidth(double szer) {
         this.szer = szer;
     }
-
 }
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Drukarnia
+
+Simple calculator for print shop pricing.
+
+## Compilation
+
+```
+javac Program/src/program/*.java
+```
+
+## Usage
+
+```
+java program.Program
+```
+
+Prices are loaded from `prices.xml`. You can edit them through option `3` in the main menu or by manually editing the XML file.
